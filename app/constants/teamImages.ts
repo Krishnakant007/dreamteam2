d51816a117fd411e76fcfb73d5af283d728ea205
Krishnakant chavan <70923432+Krishnakant007@users.noreply.github.com>
2025-04-09 14:21:31 +0530

    Update teamImages.ts

--- app/constants/teamImages.ts
+++ app/constants/teamImages.ts
@@ -1,12 +1,12 @@
 // constants/teamImages.ts
 export const IPL_TEAM_IMAGES: { [key: string]: string } = {
-    "Chennai Super Kings": "/images/CSK.png",
+   "Chennai Super Kings": "/images/CSK.png",
   "Mumbai Indians": "/images/MI.webp",
   "Kolkata Knight Riders": "/images/kkr.png",
-  "Sunrisers Hyderabad": "/images/SRH (2).png",
+  "Sunrisers Hyderabad": "/images/SRH2.png",
   "Delhi Capitals": "/images/DC.webp",
-  "Lucknow Super Giants": "/images/LSG (2).png",
-  "Rajasthan Royals": "/images/RR (2).png",
+  "Lucknow Super Giants": "/images/LSG2.png",
+  "Rajasthan Royals": "/images/RR2.png",
   "Punjab Kings": "/images/PBKS.webp",
   "Gujarat Titans": "/images/GT.webp",
   "Royal Challengers Bengaluru": "/images/rcb.png",
